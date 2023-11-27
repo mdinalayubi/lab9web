5aa4519d9d6e797be9b586d1c551862faea05e05
mdinalayubi <mdinalayubi@gmail.com>
2023-11-27 12:55:25 +0700

    Upload

---
+++ php_modular/header.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Contoh Modularisasi</title>
+    <link href="style.css" rel="stylesheet" type="text/stylesheet"media="screen" />
+</head>
+<body>
+    <div class="container">
+        <header>
+            <h1>Modularisasi Menggunakan Require</h1>
+        </header>
+        <nav>
+            <a href="home.php">Home</a>
+            <a href="about.php">About</a>
+            <a href="kontak.php">Kontak</a>
+        </nav>
